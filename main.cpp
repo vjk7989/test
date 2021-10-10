929e4100284698f0cfa5b85fa4078d69a1d43c52
dhruv <dhruvjha2002@gmail.com>
2021-10-10 14:25:04 +0530

    Added Name Feature

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 
+void name(){
+    char name[20];
+    std::cout<<std::endl<<"Enter your name: ";
+    std::cin>>name;
+    std::cout<<std::endl<<"It's good to meet you "<<name;
+}
+
 int main(){
 std::cout<<"Hello World!";
+name();
 return 0;
 }
