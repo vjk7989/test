d18fbc360ab78a121b15878fd14d57d6f22f6f97
dhruv <dhruvjha2002@gmail.com>
2021-10-10 14:28:56 +0530

    Added Splash Header

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 
+void SplashScreen(){
+    std::cout<<"RUNNING THE NEW PROGRAM\tPRE_ALPHA_BUILD V 0.0.1"<<std::endl<<std::endl;
+}
+
 int main(){
-std::cout<<"Hello World!";
-return 0;
+    SplashScreen();
+    std::cout<<"Hello World!";
+    return 0;
 }
