2ccc4e5f40bddb27bb48cca0083083166dc7a91d
dhruv <dhruvjha2002@gmail.com>
2021-10-11 11:42:04 +0530

    Added new addition feature

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 
+
+
+int AddTwo(int a, int b){
+    return a+b;
+}
+
 void SplashScreen(){
     std::cout<<"RUNNING THE NEW PROGRAM\tPRE_ALPHA_BUILD V 0.0.1"<<std::endl<<std::endl;
 }
@@ -15,5 +21,11 @@ int main(){
 SplashScreen();
 std::cout<<"Hello World!";
 name();
+int num[2];
+std::cout<<std::endl<<"Enter one number: ";
+std::cin>>num[0];
+std::cout<<std::endl<<"Enter second number: ";
+std::cin>>num[1];
+std::cout<<std::endl<<"The sum of these numbers is: "<<AddTwo(num[0],num[1]);
 return 0;
 }
